1f978403714c864614569d4e1473354565bce5b6
yangyihan16@mails.ucas.ac.cn <yangyihan16@mails.ucas.ac.cn>
2020-02-29 17:34:42 +0800

    Update calc_rtt.py

--- calc_rtt.py
+++ calc_rtt.py
@@ -4,16 +4,16 @@ import sys
 
 rttFile = "rtt.txt"
 
-total = 0
+total = 0.0
 count = 0
 
 with open(rttFile, "r") as f:
     for line in f:
         res = line.split()
-        if len(res) == 2 and res[1].isdigit():
-            total += int(res[1])
+        if len(res) == 2:
+            total += float(res[1])
             count += 1
     f.close()
-    avg = round(float(total)/float(count), 4)
+    avg = round(total/float(count), 4)
 
 print("rtt average:",avg)
