ad1a9313b63830572373f81d11b962ed44b1728e
2016k8009929004 <yangyihan16@mails.ucas.ac.cn>
2020-03-10 02:35:07 -0400

    update

--- run.sh
+++ run.sh
@@ -11,8 +11,8 @@ server_port=12345
 if test $1 = $server
 then
     rm record_core_*.txt
-    make clean
-    make server
+#    make clean
+#    make server
     ./server -N 1 -p service -f server.conf
 else
     ./client $2 $server_ip $server_port $3
