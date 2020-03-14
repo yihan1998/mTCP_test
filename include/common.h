ef9602df76be1c5834e2a6fc2efb498a4dc6f339
yangyihan16@mails.ucas.ac.cn <yangyihan16@mails.ucas.ac.cn>
2020-03-14 23:08:52 +0800

    Update common.h

--- include/common.h
+++ include/common.h
@@ -29,6 +29,29 @@
 #include "netlib.h"
 #include "debug.h"
 
+#define LL long long
+
+#define NUM_ITER 1000000000
+
+#define PUT_PERCENT 50			// Percentage of PUT operations
+
+#define NUM_KEYS M_1			// 51 * M_4 ~ 200 M keys
+#define NUM_KEYS_ M_1_
+
+#define SWAP(a,b) do{a^=b;b^=a;a^=b;}while(0)
+
+#define VALUE_SIZE 256
+#define KEY_SIZE 64
+
+// The key-value struct in network connection
+struct __attribute__((__packed__)) kv_trans_item {
+	uint16_t len;
+	uint8_t value[VALUE_SIZE];
+	uint8_t key[KEY_SIZE];
+};
+
+#define KV_ITEM_SIZE sizeof(struct kv_trans_item)
+
 struct client_arg {
     char ** ip_addr;
     int port;
