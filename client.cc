a5692d9451a406a3c21b3ef490aa8b67a945b812
yangyihan16@mails.ucas.ac.cn <yangyihan16@mails.ucas.ac.cn>
2020-03-17 15:52:13 +0800

    Update client.cc

--- client.cc
+++ client.cc
@@ -380,7 +380,7 @@ void * send_request(void * arg){
                 tot_recv += recv_size;
 
                 if(tot_recv == KV_ITEM_SIZE){
-                    if(res_kv->len == VALUE_SIZE && bufcmp((char *)req_kv->value, (char *)&value_corpus[key_j * VALUE_SIZE], VALUE_SIZE)){
+                    if(req_kv->len == VALUE_SIZE && bufcmp((char *)req_kv->value, (char *)&value_corpus[key_j * VALUE_SIZE], VALUE_SIZE)){
                         printf("[CLIENT] GET success! key: %.*s, value: %.*s\n", KEY_SIZE, req_kv->key, VALUE_SIZE, req_kv->value);
                         match_search++;
                     }else{
