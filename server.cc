5c9d3b8f4ed0e860dba0dd259039f485286b35b0
yangyihan16@mails.ucas.ac.cn <yangyihan16@mails.ucas.ac.cn>
2020-03-20 19:42:25 +0800

    Update server.cc

--- server.cc
+++ server.cc
@@ -314,7 +314,7 @@ int HandleReadEvent(struct thread_context *ctx, int thread_id, int sockid, struc
 	read_time += (int)(end_time - start_time);
 #endif
 
-    return sent;
+    return len;
 }
 
 int AcceptConnection(struct thread_context *ctx, int listener){
