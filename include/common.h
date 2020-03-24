3887566a9ed425fa70bf65e893a5693e9af8c450
yangyihan16@mails.ucas.ac.cn <yangyihan16@mails.ucas.ac.cn>
2020-03-24 21:32:58 +0800

    Update common.h

--- include/common.h
+++ include/common.h
@@ -31,8 +31,8 @@
 
 #define REPLY_SIZE 50
 
-#define NUM_KEYS K_128			// 51 * M_4 ~ 200 M keys
-#define NUM_KEYS_ K_128_
+#define NUM_KEYS K_256			// 51 * M_4 ~ 200 M keys
+#define NUM_KEYS_ K_256_
 
 #define SWAP(a,b) do{a^=b;b^=a;a^=b;}while(0)
 
