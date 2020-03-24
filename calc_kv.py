b5d54115618bd7df3419a2934d3bbbe549adf6ba
yangyihan16@mails.ucas.ac.cn <yangyihan16@mails.ucas.ac.cn>
2020-03-24 15:12:21 +0800

    Update calc_kv.py

--- calc_kv.py
+++ calc_kv.py
@@ -3,13 +3,17 @@ import os
 import sys
 
 with open(str(sys.argv[1]), "r") as f:
-    total = 0.0
+    total_put = 0.0
+    total_get = 0.0
     count = 0
     for line in f:
-        res = line.strip()
-        total += float(res)
-        count += 1
+        res = line.split()
+        if len(res) == 4:
+           total_put += float(res[1])
+           total_get += float(res[3])
+           count += 1
     f.close()
-    avg = round(total/float(count), 4)
+    avg_put = round(total_put/float(count), 4)
+    avg_get = round(total_get/float(count), 4)
 
-print('throughput avg: %.4f' % (avg))
+print('put avg: %.4f, get avg: %.4f' % (avg_put, avg_get))
