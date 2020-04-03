a43c8b6a25358306414aa814bf541abe566764db
yangyihan16@mails.ucas.ac.cn <yangyihan16@mails.ucas.ac.cn>
2020-04-03 17:45:45 +0800

    update

--- server.cc
+++ server.cc
@@ -441,10 +441,11 @@ int HandleReadEvent(struct thread_context *ctx, int thread_id, int sockid, struc
 
         int i;
 		for(i = 0;i < key_num;i++){
-            //printf(" >> GET key: %.*s\n", KEY_SIZE, recv_item + i * KEY_SIZE);
+            printf(" >> GET key: %.*s\n", KEY_SIZE, recv_item + i * KEY_SIZE);
             //char * buff = (char *)malloc(1024);
 			res = hi->search(thread_id, (uint8_t *)(recv_item + i * KEY_SIZE), (uint8_t *)(value + i * VALUE_LENGTH));
-            if(res == true){
+            printf(" >> GET value: %.*s\n", VALUE_LENGTH, value + i * VALUE_LENGTH);
+			if(res == true){
                 //memcpy(value + i * VALUE_LENGTH, buff, VALUE_LENGTH);
             }else{
                 //printf(" >> GET failed\n");
