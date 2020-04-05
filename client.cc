52bf24c8b01b8f0731f9d97c6b6f21f1020358e9
yangyihan16@mails.ucas.ac.cn <yangyihan16@mails.ucas.ac.cn>
2020-04-05 18:54:29 +0800

    Update client.cc

--- client.cc
+++ client.cc
@@ -352,7 +352,7 @@ void * send_request(void * arg){
     FILE * fp = fopen("rtt.txt", "a+");
     fseek(fp, 0, SEEK_END);
 #endif
-
+/*
 //[Version 3.0 - mixed tests]
     for(iter = 0, key_i = 0, key_j = 0;iter < num_kv;iter++){
         if(iter < num_put_kv) {
@@ -462,8 +462,8 @@ void * send_request(void * arg){
             free(value);
 		}
     }
+*/
 
-/*
 //[Version 4.0 - 256B batched key] 
     for(iter = 0, key_i = 0, key_j = 0;iter < num_kv;){
         if(iter < num_put_kv) {
@@ -582,7 +582,7 @@ void * send_request(void * arg){
             free(value);
 		}
     }
-*/
+
 #ifdef __EV_RTT__
     int j;
     for(j = 0;j < request_cnt;j++){
