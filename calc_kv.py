bb630032639eed95587e3bb94b212d4f2d33d538
yangyihan16@mails.ucas.ac.cn <yangyihan16@mails.ucas.ac.cn>
2020-04-28 15:57:01 +0800

    Update calc_kv.py

--- calc_kv.py
+++ calc_kv.py
@@ -5,15 +5,18 @@ import sys
 with open(str(sys.argv[1]), "r") as f:
     total_put = 0.0
     total_get = 0.0
+    total_scan = 0.0
     count = 0
     for line in f:
         res = line.split()
-        if len(res) == 4:
+        if len(res) == 6:
            total_put += float(res[1])
            total_get += float(res[3])
+           total_scan += float(res[5])
            count += 1
     f.close()
     avg_put = round(total_put/float(count), 4)
     avg_get = round(total_get/float(count), 4)
+    avg_scan = round(total_scan/float(count), 4)
 
-print('put avg: %.4f, get avg: %.4f' % (avg_put, avg_get))
+print('put avg: %.4f, get avg: %.4f, scan avg: %.4f' % (avg_put, avg_get, avg_scan))
