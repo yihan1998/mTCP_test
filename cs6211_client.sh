6ef63fc41bfd275dac487b6803bbf39addc60c5e
yihan <yangyihan16@mails.ucas.ac.cn>
2021-03-23 10:51:16 +0800

    cs6211 test

---
+++ cs6211_client.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+trap 'exit' TERM
+
+server_ip='10.0.0.2'
+server_port=80
+
+echo -n "Buffer size(B): "
+read buff_size
+#buff_size=1024
+
+#echo -n "number of CPU cores: "
+#read num_core
+max_cores=10
+
+echo -n "Total test time(s): "
+read test_time
+
+echo -n "Benchmark type[open/close loop]?: "
+read test_mode
+
+echo -n "Record Round Trip Time[yes/no]?: "
+read record_rtt
+
+echo -n "Number of jobs: "
+read num_jobs
+
+if [[ "$record_rtt" == *"yes"* ]];then
+    echo " >> evaluting Round Trip Time"
+    eval_rtt=1
+else
+    eval_rtt=0
+fi
+
+rm rtt_*.txt
+
+make clean && make client RTT=$eval_rtt
+
+#echo -n "number of connections: "
+#read num_connection
+
+for j in $(seq 4 $num_jobs)
+do
+    num_flow=1
+    num_cores=$max_cores
+    total_conn=$( expr "$num_cores" '*' "$num_flow" )
+
+    echo "Testing RTT for $total_conn connections on $num_cores core(s), each have $num_flow connection(s) ..."
+
+    ./client    --num_cores=$num_cores \
+                --num_flow=$num_flow \
+                --size=$buff_size \
+                --time=$test_time \
+                --server_ip=$server_ip \
+                --server_port=$server_port \
+                --test_mode=$test_mode
+    
+    wait
+
+    echo "Test done"
+
+    sleep 5
+
+    if [ $eval_rtt -eq 1 ]
+    then
+        total=`expr $num_cores \* $num_flow`
+        python merge_file.py $total
+    fi
+done
---
+++ cs6211_server.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+trap 'exit' TERM
+
+echo -n "Buffer size(B): "
+read buff_size
+#buff_size=1024
+
+echo -n "number of CPU cores: "
+read num_cores
+#num_cores=1
+
+echo -n "Total test time(s): "
+read test_time
+
+echo -n "benchmark type[open/close loop]?: "
+read test_mode
+
+echo -n "Number of jobs: "
+read num_jobs
+
+#echo -n "number of connections: "
+#read num_connection
+
+make clean && make server 
+
+rm throughput_*.txt
+
+for j in $(seq 4 $num_jobs)
+do
+    sed -i '/rcvbuf/d' server.conf
+    sed -i '/sndbuf/d' server.conf
+
+    queue_size=`expr 1024 \* $j`
+
+    sed -i '$a rcvbuf = $queue_size' server.conf
+    sed -i '$a sndbuf = $queue_size' server.conf
+
+    num_connection=`echo "2^$j" | bc `
+
+    echo "Testing RTT for $num_connection connections..."
+
+    ./server    --num_cores=$num_cores \
+                --size=$buff_size \
+                --time=$test_time \
+                --test_mode=$test_mode 
+
+    wait
+
+    echo "Test done"
+done
