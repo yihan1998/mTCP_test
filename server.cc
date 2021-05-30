773b328720d92af1ac50460a05274cedd9f30833
yihan <yangyihan16@mails.ucas.ac.cn>
2021-05-30 12:50:48 +0800

    TEST

--- server.cc
+++ server.cc
@@ -265,30 +265,30 @@ ServerSignalHandler(int signum) {
 				ev.data.sockid = socket;
 				mtcp_epoll_ctl(ctx->mctx, ctx->ep, MTCP_EPOLL_CTL_ADD, socket, &ev);
 
-			printf(" >> receive SIGQUIT signal\n");
+				printf(" >> receive SIGQUIT signal\n");
 
-			for (i = 0; i < num_cores; i++) {
-			if (app_thread[i] != pthread_self()) {
-				int kill_rc = pthread_kill(app_thread[i], 0);
+				for (int i = 0; i < num_cores; i++) {
+					if (app_thread[i] != pthread_self()) {
+						int kill_rc = pthread_kill(app_thread[i], 0);
 
-				if (kill_rc == ESRCH) {
-					printf("the specified thread did not exists or already quit\n");
-				}else if(kill_rc == EINVAL) {
-					printf("signal is invalid\n");
-				}else{
-					printf("the specified thread is alive\n");
-					int ret = pthread_kill(app_thread[i], SIGQUIT);
-					if (ret == EINVAL) {
-						printf("Invalid signal\n");
-					} else if (ret == ESRCH) {
-						printf("No thread os found\n");
-					} else {
-						printf("succeed!\n");
+						if (kill_rc == ESRCH) {
+							printf("the specified thread did not exists or already quit\n");
+						}else if(kill_rc == EINVAL) {
+							printf("signal is invalid\n");
+						}else{
+							printf("the specified thread is alive\n");
+							int ret = pthread_kill(app_thread[i], SIGQUIT);
+							if (ret == EINVAL) {
+								printf("Invalid signal\n");
+							} else if (ret == ESRCH) {
+								printf("No thread os found\n");
+							} else {
+								printf("succeed!\n");
+								//pthread_kill(app_thread[i], SIGTERM);
+							}
+						}
 					}
 				}
-			}
-		}
-	}
 	
 				mtcp_destroy_context(mctx);
 				pthread_exit(NULL);
