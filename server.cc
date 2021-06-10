1e58dcf85edb47ed7f4a759495b1efdaaa7b99e5
yangyihan <yangyihan1998@gmail.com>
2021-06-10 12:05:20 +0800

    update

--- server.cc
+++ server.cc
@@ -466,7 +466,7 @@ void * RunServerThread(void *arg){
 
 		struct timeval current;
 		gettimeofday(&current, NULL);
-		if(current.tv_sec - start.tv_sec >= execution_time + 2) {
+		if(current.tv_sec - start.tv_sec >= execution_time + 5) {
 			fprintf(stdout, " [%s] Time's up! End connections\n", __func__);
             CloseConnection(ctx, listener, &ctx->svars[listener]);
             done[core] = TRUE;
